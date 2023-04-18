6fa410851b105e7718ef49feb95349248c62e988
longzhan.huang <longzhan.huang@thoughtworks.com>
2023-04-18 12:11:55 +0800

    add logo

---
+++ README.md
@@ -0,0 +1,47 @@
+# export-helper README
+
+Welcome to the README for your extension "export-helper"! This extension provides a variety of features to help you export your files in a snap. With export-helper, you can easily export files in various formats, customize export settings, and even export multiple files at once! 😎
+
+## Features
+
+- Export files in various formats 📁
+- Customize export settings ⚙️
+- Export multiple files at once 🚀
+
+## Requirements
+
+To use export-helper, you'll need to have VS Code version 1.50.0 or higher installed on your machine. 💻
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+* `exportHelper.format`: The format to export files in.
+* `exportHelper.path`: The path to export files to.
+* `exportHelper.includeSubfolders`: Whether to include subfolders when exporting multiple files.
+
+## Known Issues
+
+There are currently no known issues with this extension. Enjoy! 😊
+
+## Release Notes
+
+### 1.0.0
+
+---
+
+## Following extension guidelines
+
+If you're interested in creating your own extension, be sure to read through the extension guidelines and follow the best practices for creating your extension. 📖
+
+* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
+
+## For more information
+
+To learn more about Markdown and how to use it in VS Code, check out the following resources:
+
+* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
+* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+
+Thanks for using export-helper! We hope it makes your life a little bit easier. 😊
+
---
+++ src/extension.ts
@@ -0,0 +1,131 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+const exportCodeActionKind = vscode.CodeActionKind.Empty.append('exportVariable');
+const exportDefaultCodeActionKind = vscode.CodeActionKind.Empty.append('exportDefaultVariable');
+
+const exportActionProvider = {
+	provideCodeActions: (document: vscode.TextDocument, range: vscode.Range) => {
+		const line = document.lineAt(range.start.line);
+		const variable = getVariableName(line.text);
+		if (variable && isValidFunctionOrVariableName(variable)) {
+			const exportAction = new vscode.CodeAction(`Export variable "${variable}"`, exportCodeActionKind);
+			exportAction.command = {
+				command: 'exportVariable',
+				arguments: [variable],
+				title: 'Export it?',
+			};
+			const exportDefaultAction = new vscode.CodeAction(`Export default "${variable}"`, exportDefaultCodeActionKind);
+			exportDefaultAction.command = {
+				command: 'exportDefaultVariable',
+				arguments: [variable],
+				title: 'Export default it?',
+			};
+			return [exportAction, exportDefaultAction];
+		}
+	},
+};
+
+const hoverProvider = {
+	provideHover(document: { lineAt: (arg0: any) => any; }, position: any) {
+		const line = document.lineAt(position);
+		const variable = getVariableName(line.text);
+		if (variable && isValidFunctionOrVariableName(variable)) {
+			// Create a MarkdownString with links to export the variable
+			const content = new vscode.MarkdownString();
+			content.appendMarkdown(`🎉 **Hooray! Export this variable:** \n\n[👉 Export]`);
+			content.appendMarkdown(`(command:exportVariable?${encodeURIComponent(JSON.stringify([variable]))})\n\n`);
+			content.appendMarkdown(`🚀 **Blast off! Export this variable as default:** \n\n[👉 Export Default]`);
+			content.appendMarkdown(`(command:exportDefaultVariable?${encodeURIComponent(JSON.stringify([variable]))})`);
+
+			// Allow the MarkdownString to be trusted
+			content.isTrusted = true;
+
+			let hover = new vscode.Hover(content);
+			return hover;
+		}
+	}
+};
+
+const disposableHoverProvider1 = vscode.languages.registerHoverProvider({ language: 'typescript', scheme: 'file' }, hoverProvider);
+const disposableHoverProvider2 = vscode.languages.registerHoverProvider({ language: 'javascript', scheme: 'file' }, hoverProvider);
+const disposableCodeActionsProvider1 = vscode.languages.registerCodeActionsProvider({ language: 'javascript', scheme: 'file' }, exportActionProvider);
+const disposableCodeActionsProvider2 = vscode.languages.registerCodeActionsProvider({ language: 'typescript', scheme: 'file' }, exportActionProvider);
+const disposableExportVariable = vscode.commands.registerCommand('exportVariable', (variableName) => {
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		vscode.window.showErrorMessage('No active text editor found');
+		return;
+	}
+	const edit = new vscode.WorkspaceEdit();
+	const lastLine = editor.document.lineAt(editor.document.lineCount - 1);
+	if (variableName) {
+		const exportLine = `\nexport { ${variableName} };`;
+		edit.insert(editor.document.uri, new vscode.Position(lastLine.range.end.line, lastLine.range.end.character), exportLine);
+		vscode.workspace.applyEdit(edit).then(() => {
+		});
+	}
+});
+
+const disposableExportDefaultVariable = vscode.commands.registerCommand('exportDefaultVariable', (variableName) => {
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		vscode.window.showErrorMessage('No active text editor found');
+		return;
+	}
+	const edit = new vscode.WorkspaceEdit();
+	const lastLine = editor.document.lineAt(editor.document.lineCount - 1);
+	if (variableName) {
+		const exportLine = `\nexport default ${variableName};`;
+		edit.insert(editor.document.uri, new vscode.Position(lastLine.range.end.line, lastLine.range.end.character), exportLine);
+		vscode.workspace.applyEdit(edit).then(() => {
+		});
+	}
+});
+
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(disposableHoverProvider1, disposableHoverProvider2, disposableCodeActionsProvider1, disposableCodeActionsProvider2, disposableExportVariable, disposableExportDefaultVariable);
+}
+
+export function deactivate() { }
+
+function isValidFunctionOrVariableName(word: string): boolean {
+	// Regular expression to match variable declaration keywords
+	const declarationRegex = /^(const|class|let|var|function)$/;
+
+	// Check if the word is a variable declaration keyword
+	if (declarationRegex.test(word)) {
+		return false;
+	}
+
+	// Check if the word starts with a number
+	if (/^[0-9]/.test(word)) {
+		return false;
+	}
+
+	// Check if the word contains any invalid characters
+	if (/[^a-zA-Z0-9_$]/.test(word)) {
+		return false;
+	}
+
+	// If the word passes all checks, it is considered a valid function or variable name
+	return true;
+}
+
+function getVariableName(text: string): string | null {
+	// Use a regular expression to match variable declarations
+	const declarationRegex = /^(const|class|let|var|function)\s+([a-zA-Z_$][0-9a-zA-Z_$]*)/;
+
+	// Match the variable name in the text using the regular expression
+	const match = text.match(declarationRegex);
+
+	// If a match is found, return the variable name from the second capture group
+	if (match) {
+		return match[2];
+	}
+
+	// If no match is found, return null
+	return null;
+}
+
